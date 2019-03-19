901621393165722dad12815c354edc3c7182fd53
Marek Ryznar <mryznar17@gmail.com>
2019-03-19 21:34:30 +0100

    Add Configuration class to create beans from external Jars

---
+++ src/main/java/com/mryznar/joke/jokeapp/joke/ChuckConfiguration.java
@@ -0,0 +1,18 @@
+package com.mryznar.joke.jokeapp.joke;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+    When class is coming from external Jar @Configuration can be used to create
+    beans from such classes
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
